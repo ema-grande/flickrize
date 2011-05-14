884dbce8ad0f8f85dcdaa49e0c2a8262206083d8
Emanuele Grande <caccolangrifata@gmail.com>
2011-05-14 18:58:16 +0200

    add configurable source photo path

--- flickrize.sh
+++ flickrize.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #Read README file before run this script!
 
-#TODO set from conf file
-photosourcepath="upload.txt"
+photosourcepath=$(cat conf | grep SOURCE | cut -d= -f2)
 
 cat ${photosourcepath} | while read num color pos title;do ./toFlickr.sh --color=$color --pos=$pos --title=$title $num;done
