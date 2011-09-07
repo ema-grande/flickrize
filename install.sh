944eb810729f34fd54018d0084da9075e6933259
Emanuele Grande <caccolangrifata@gmail.com>
2011-09-07 13:07:49 +0200

    Add creation of 'upload.txt'

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 #Prepare the envoirement for flickrize script
 #very basic XD
 
 chmod +x *.sh
-#mkdir $(cat conf | grep RESPHOTOF | cut -d= -f2) 	#useless done by toFlickr.sh
+
+SOURCE=$(cat conf | grep DEBUG | cut -d= -f2)
+if [ ! -a $SOURCE ]
+then
+	echo "<photo file path> <sign color> <sign pos> <title>" > $SOURCE
+fi
+
+#TODO some more things :)
